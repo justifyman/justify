8f82b789b84536cce879d58a6fe77d0888bed417
justify <143286685+justifyman@users.noreply.github.com>
2025-07-07 16:31:02 +0400

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -72,7 +72,7 @@ const About: FC = () => {
             </div>
             <div className="space-y-1 md:space-y-2">
               <h3 className="text-[#7c8fff] text-3xl md:text-5xl font-bold">
-                6+
+                4+
               </h3>
               <p className="text-gray-400 text-sm md:text-base">Technologies mastered</p>
             </div>
