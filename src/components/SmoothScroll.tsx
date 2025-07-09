5253b0265bf4b17983e51f8aa7eb2110a85d9b2f
justify <143286685+justifyman@users.noreply.github.com>
2025-07-09 16:10:27 +0400

    Updated package-lock.json

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, ReactNode } from 'react';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 
 interface SmoothScrollProps {
   children: ReactNode;
@@ -9,10 +9,11 @@ const SmoothScroll: FC<SmoothScrollProps> = ({ children }) => {
   useEffect(() => {
     // Initialize Lenis
     const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       mouseMultiplier: 1,
       smoothTouch: false,
       touchMultiplier: 2,
-      infinite: false,
     });
 
     // Animation loop
